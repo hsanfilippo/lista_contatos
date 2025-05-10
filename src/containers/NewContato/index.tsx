d15b71c5e98642ed9e2655dcf854a4c16d85e6f3
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-10 11:54:01 -0300

    feat: implementando troca de estado nos campos e action ao submeter form

--- src/containers/NewContato/index.tsx
+++ src/containers/NewContato/index.tsx
@@ -1,8 +1,6 @@
 import { FormEvent, useState } from 'react'
 import { useDispatch } from 'react-redux'
 
-import Contato from '../../models/Contato'
-
 import { formActions } from '../../store/reducers/formulario'
 import { addContato } from '../../store/reducers/contatos'
 import HeaderNew from '../../components/HeaderNew'
@@ -15,7 +13,7 @@ const NewContato = () => {
   const [nome, setNome] = useState('')
   const [apelido, setApelido] = useState('')
   const [email, setEmail] = useState('')
-  const [tel, setTel] = useState(0)
+  const [tel, setTel] = useState('')
 
   const adicionarContato = (e: FormEvent) => {
     e.preventDefault()
@@ -35,15 +33,47 @@ const NewContato = () => {
   return (
     <>
       <HeaderNew />
-      <S.Form>
+      <S.Form onSubmit={adicionarContato}>
         <S.Label htmlFor="nome">Nome</S.Label>
-        <S.Input placeholder="texto de teste 1234" type="text" name="nome" />
+        <S.Input
+          placeholder="texto de teste 1234"
+          type="text"
+          name="nome"
+          value={nome}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setNome(e.target.value)
+          }
+        />
         <S.Label htmlFor="apelido">Apelido</S.Label>
-        <S.Input placeholder="texto de teste 1234" type="text" name="apelido" />
+        <S.Input
+          placeholder="texto de teste 1234"
+          type="text"
+          name="apelido"
+          value={apelido}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setApelido(e.target.value)
+          }
+        />
         <S.Label htmlFor="email">E-mail</S.Label>
-        <S.Input placeholder="texto de teste 1234" type="email" name="email" />
+        <S.Input
+          placeholder="texto de teste 1234"
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setEmail(e.target.value)
+          }
+        />
         <S.Label htmlFor="tel">Telefone</S.Label>
-        <S.Input placeholder="texto de teste 1234" type="tel" name="tel" />
+        <S.Input
+          placeholder="texto de teste 1234"
+          type="tel"
+          name="tel"
+          value={tel}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setTel(e.target.value)
+          }
+        />
         <RegularButton type="submit">Salvar</RegularButton>
       </S.Form>
     </>
