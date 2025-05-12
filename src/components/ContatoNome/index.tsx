3b6dc1252540a8b0a09aebfa25789f951e6ca46b
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-11 22:10:04 -0300

    feat: adicionando roteamento para a pag. do contato ao clicar

--- src/components/ContatoNome/index.tsx
+++ src/components/ContatoNome/index.tsx
@@ -3,8 +3,10 @@ import { RootReducer } from '../../store'
 
 import { HiMail, HiPhone } from 'react-icons/hi'
 import * as S from './styles'
+import { useNavigate } from 'react-router-dom'
 
 const Contato = () => {
+  const navigate = useNavigate()
   const { itens } = useSelector((state: RootReducer) => state.contatos)
   const contatosOrdenados = [...itens].sort((a, b) =>
     (a.nome ?? '').localeCompare(b.nome ?? '')
@@ -14,15 +16,16 @@ const Contato = () => {
     <ul>
       {contatosOrdenados.map((contato) => (
         <>
-          <S.NomeContainer key={contato.id}>
-            <S.Nome>
-              {contato.nome}
-              {contato.apelido ? (
-                <S.Apelido>&quot;{contato.apelido}&quot;</S.Apelido>
-              ) : (
-                ''
-              )}
-            </S.Nome>
+          <S.NomeContainer
+            key={contato.id}
+            onClick={() => navigate(`/contatos/${contato.id}`)}
+          >
+            <S.Nome>{contato.nome}</S.Nome>
+            {contato.apelido ? (
+              <S.Apelido>&quot;{contato.apelido}&quot;</S.Apelido>
+            ) : (
+              ''
+            )}
             <S.IconesContainer>
               {contato.email ? (
                 <S.Icone>
