e22911d79833d7ffca020b455162192752cf11d9
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-11 21:14:44 -0300

    feat: pagina para exibir contato por ID

---
+++ src/pages/ContatoId/index.tsx
@@ -0,0 +1,13 @@
+import ContatoViewEdit from '../../containers/ContatoViewEdit'
+import Header from '../../components/Header'
+
+const ContatoId = () => {
+  return (
+    <>
+      <Header />
+      <ContatoViewEdit />
+    </>
+  )
+}
+
+export default ContatoId
