1e7de6e3c3c74be2e1061a5ed49617c72d42466d
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-11 22:09:34 -0300

    feat: estilizacao da pagina individual de contato

--- src/containers/ContatoViewEdit/index.tsx
+++ src/containers/ContatoViewEdit/index.tsx
@@ -1,6 +1,9 @@
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { useState } from 'react'
+
 import { RootReducer } from '../../store'
+import * as S from './styles'
 
 const ContatoViewEdit = () => {
   const { id } = useParams<{ id: string }>()
@@ -8,19 +11,47 @@ const ContatoViewEdit = () => {
     state.contatos.itens.find((c) => c.id === Number(id))
   )
 
+  const [isEditando, setIsEditando] = useState(false)
+
   return (
     <>
       {contato ? (
         <>
-          <h1>Visualizar/Editar Contato</h1>
-          <p>Nome: {contato.nome}</p>
-          <p>Apelido: {contato.apelido}</p>
-          <p>Email: {contato.email}</p>
-          <p>Telefone: {contato.tel}</p>
-          <p>ID: {contato.id}</p>
+          <S.CardContainer>
+            <S.Card>
+              <S.Lista>
+                <S.ItemDaLista>
+                  <S.Nome>{contato.nome}</S.Nome>
+                </S.ItemDaLista>
+                <S.ItemDaLista>
+                  <S.Apelido>&quot;{contato.apelido}&quot;</S.Apelido>
+                </S.ItemDaLista>
+                <S.ItemDaLista>
+                  <S.Informacoes>{contato.email}</S.Informacoes>
+                </S.ItemDaLista>
+                <S.ItemDaLista>
+                  <S.Informacoes>{contato.tel}</S.Informacoes>
+                </S.ItemDaLista>
+              </S.Lista>
+              {isEditando ? (
+                <S.ButtonContainer>
+                  <S.SaveButton onClick={() => setIsEditando(false)}>
+                    Salvar
+                  </S.SaveButton>
+                </S.ButtonContainer>
+              ) : (
+                <S.ButtonContainer>
+                  <S.DeleteButton>Excluir contato</S.DeleteButton>
+                  <S.EditButton onClick={() => setIsEditando(true)}>
+                    Editar
+                  </S.EditButton>
+                </S.ButtonContainer>
+              )}
+            </S.Card>
+          </S.CardContainer>
         </>
       ) : (
-        <h1>Nada encontrado aqui</h1>
+        <h1>Contato não encontrado</h1>
       )}
     </>
   )
---
+++ src/containers/ContatoViewEdit/styles.ts
@@ -0,0 +1,80 @@
+import styled from 'styled-components'
+import { RegularButton } from '../../styles'
+
+export const CardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 40px;
+`
+
+export const Card = styled.div`
+  background-color: #cccccc;
+  text-align: center;
+  width: 600px;
+  max-width: 80vw;
+  border-radius: 8px;
+  box-shadow: 0px 2px 16px rgba(0, 0, 0, 0.4);
+  padding: 24px;
+
+  ul li {
+    background-color: transparent;
+  }
+`
+
+export const Lista = styled.ul``
+
+export const ItemDaLista = styled.li`
+  margin-bottom: 16px;
+`
+
+export const Nome = styled.h2`
+  color: #3a3a3a;
+`
+
+export const Apelido = styled.h4`
+  font-weight: lighter;
+  color: #6b6b6b;
+`
+
+export const Informacoes = styled.p`
+  color: #6b6b6b;
+`
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  margin-top: 40px;
+`
+
+export const DeleteButton = styled(RegularButton)`
+  color: red;
+  background-color: transparent;
+  border-color: red;
+
+  &:hover {
+    background-color: rgb(255, 0, 0, 0.1);
+    transition: ease 0.5s;
+  }
+`
+
+export const EditButton = styled(RegularButton)`
+  color: #8944cf;
+  background-color: transparent;
+
+  &:hover {
+    background-color: rgba(137, 68, 207, 0.3);
+    transition: ease 0.5s;
+  }
+`
+
+export const SaveButton = styled(RegularButton)`
+  color: #b6ff86;
+  background-color: transparent;
+  border-color: #b6ff86;
+
+  &:hover {
+    background-color: rgb(182, 255, 134, 0.1);
+    transition: ease 0.5s;
+  }
+`
